061c6be0242bca64266a9500ce5363d617b2b81b
scfhome <scfhome@SCFDESKTOP>
2017-01-24 19:41:47 +0100

    1

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -8,18 +8,19 @@ using Microsoft.AspNetCore.Builder;
 
 namespace WebApplication1
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+      var host = new WebHostBuilder()
+          .UseKestrel()
+          .UseContentRoot(Directory.GetCurrentDirectory())
+          .UseIISIntegration()
+          .UseStartup<Startup>()
+          .UseUrls("http://*:8080")
+          .Build();
 
-            host.Run();
-        }
+      host.Run();
     }
+  }
 }
